7ac061aa95b3bf1d31c44e3f857ec95872bffce2
issabella988 <isabellavilcu@yahoo.com>
2020-12-16 00:04:16 +0200

    tema

--- app/src/main/java/ro/ac/upt/christmasarsample/AugmentedActivity.kt
+++ app/src/main/java/ro/ac/upt/christmasarsample/AugmentedActivity.kt
@@ -1,13 +1,9 @@
 package ro.ac.upt.christmasarsample
 
-import android.app.Activity
-import android.app.ActivityManager
-import android.content.Context
 import android.net.Uri
-import android.os.Build
 import android.support.v7.app.AppCompatActivity
+import android.os.Build
 import android.os.Bundle
-import android.util.Log
 import android.widget.Toast
 import com.google.ar.core.Anchor
 import com.google.ar.core.HitResult
@@ -35,6 +31,14 @@ class AugmentedActivity : AppCompatActivity() {
         TODO("2. Invoke addRenderableToScene once a tap is executed over the AR plane")
 
 
+        arFragment.setOnTapArPlaneListener { hitResult, plane, motionEvent ->
+            val anchor = hitResult.createAnchor()
+            //anchor.setParent(arFragment.arSceneView.scene)
+            addRenderableToScene(anchor, renderable)
+
+        }
+
+
     }
 
     private fun initRenderableModel() {
@@ -42,17 +46,42 @@ class AugmentedActivity : AppCompatActivity() {
 
         TODO("1. Init model renderable variable")
 
+        ModelRenderable.builder()
+            .setSource(this, modelUri)
+
+            .build()
+            .thenAccept{renderable = it}
+
+
+          // .exceptionally
+         //  {
+           //    Toast.makeText(this, "Unable to load andy renderable", Toast.LENGTH_LONG)
+            //   null
+
+
+
+         //  }
 
     }
 
-    private fun addRenderableToScene(anchor: Anchor, renderable: Renderable) {
+
+
+
+    private fun addRenderableToScene(anchor: Anchor, renderable: Renderable?) {
         TODO("3. Build an anchor node and set the AR scene to be its parent")
+
+        val anchorNode = AnchorNode(anchor)
+        anchorNode.setParent(arFragment!!.arSceneView.scene)
 
 
         TODO("4. Build an transformable node and set the previously anchor node to be its parent")
 
+        val transformableNode = TransformableNode(arFragment!!.transformationSystem)
+
+        transformableNode.setParent(anchorNode)
 
         TODO("5. Assign node's renderable property to previously loaded renderable")
+        transformableNode.renderable = renderable
 
 
     }
